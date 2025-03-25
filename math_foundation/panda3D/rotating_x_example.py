3dc292073035bca8a090242b7558a01e4a1a6357
endsley <chieh.t.wu@gmail.com>
2025-03-25 09:41:25 -0400

    kk

--- math_foundation/panda3D/rotating_x_example.py
+++ math_foundation/panda3D/rotating_x_example.py
@@ -9,22 +9,17 @@ class rotating_point(space):
 	def __init__(self):
 		space.__init__(self)
 		self.v = self.create_vector((1,1,1))
-
 		self.taskMgr.add(self.rotate_x, "Rotate")
 
 	def rotate_x(self, task):
 		v = self.v
 		θ = π/256
-
 		R = np.array([	[1, 0, 0],
 				[0, cos(θ), -sin(θ)],
 				[0, sin(θ), cos(θ)]])
-		
 		new_loc = R @ v.pos
 		v.redraw(new_loc)
 		return Task.cont
-
-
 
 app = rotating_point()
 app.run()
