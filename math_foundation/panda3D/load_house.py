58a3f0cfb77794ea9909aa1194a2732a5f9890a6
endsley <chieh.t.wu@gmail.com>
2025-03-25 23:17:59 -0400

    kk

---
+++ math_foundation/panda3D/load_house.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from space import *
+from panda3d.core import loadPrcFileData, AmbientLight, DirectionalLight, PointLight, Spotlight, Vec4, Vec3
+loadPrcFileData("", "win-size 800 600") #optional, set window size
+loadPrcFileData("", "window-title My Panda3D Scene") #optional, set window title
+
+from direct.showbase.ShowBase import ShowBase
+
+
+class load_house(space):
+    def __init__(self):
+        space.__init__(self)
+        self.house = self.loader.loadModel("house_mesh2.glb")
+        #self.house.setR(90)
+        #self.house.setH(90)
+        #self.house.setP(90)
+        self.house.reparentTo(self.render)
+
+
+        # Add Ambient Light (fills the scene with a general light)
+        ambientLight = AmbientLight("ambientLight")
+        ambientLight.setColor(Vec4(0.2, 0.2, 0.2, 1)) # Dim white light
+        self.render.setLight(self.render.attachNewNode(ambientLight))
+        
+        # Add Directional Light (simulates sunlight)
+        directionalLight = DirectionalLight("directionalLight")
+
+
+        directionalLight.setColor(Vec4(1, 1, 0.8, 1)) # Slightly yellow light
+        directionalLight.setDirection(Vec3(0, 0, -1)) # Corrected: Using Vec3
+        self.render.setLight(self.render.attachNewNode(directionalLight))
+
+app = load_house()
+app.run()
