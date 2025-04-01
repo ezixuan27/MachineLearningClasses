c21da1176468c70f029dea04aaa69284e3c55830
endsley <chieh.t.wu@gmail.com>
2025-03-31 21:38:18 -0400

    kk

--- math_foundation/lecture_6_determinants/determinant_usage.py
+++ math_foundation/lecture_6_determinants/determinant_usage.py
@@ -3,9 +3,9 @@
 import numpy as np
 
 # Command to find determinant
-A = np.array([	[3,2,1],
-				[1,1,1],
-				[1,2,1]])
+A = np.array([[3,2,1],
+			[1,1,1],
+			[1,2,1]])
 
 d = np.linalg.det(A)
 print(d)
