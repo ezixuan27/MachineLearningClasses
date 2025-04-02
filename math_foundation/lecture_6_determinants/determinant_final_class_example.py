913ed289618b25be183670213feab8e0e66eb1df
endsley <chieh.t.wu@gmail.com>
2025-04-02 10:05:48 -0400

    kk

--- math_foundation/lecture_6_determinants/determinant_final_class_example.py
+++ math_foundation/lecture_6_determinants/determinant_final_class_example.py
@@ -3,12 +3,8 @@
 import numpy as np
 
 def calculate_determinant_2_inverse_d(d_matrix):
-    """Calculates the determinant of 2 * (D^-1)."""
-    # Calculate the inverse of D
     d_inverse = np.linalg.inv(d_matrix)
-    # Calculate 2 * D^-1
     two_inverse_d = 2 * d_inverse
-    # Calculate the determinant of 2 * D^-1
     determinant = np.linalg.det(two_inverse_d)
     return determinant
 
