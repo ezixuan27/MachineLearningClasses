a6a61eb1202867213209cd7a127b3a010b35aaf4
endsley <chieh.t.wu@gmail.com>
2025-05-08 12:07:34 -0400

    kk

--- math_foundation/lecture_13/imputation.py
+++ math_foundation/lecture_13/imputation.py
@@ -2,47 +2,30 @@
 
 import numpy as np
 from numpy.linalg import eigh
+from numpy import genfromtxt
 
-n = 1000
-# Step 1: Create a low-rank matrix A
-A = np.random.randn(n,n)  # A will be 3000x3000 but rank 10
-[U,σ,Vᵀ] = np.linalg.svd(A)  # eigh because B is symmetric
-Σ = np.zeros((n,n))
-Σ[0,0] = 100
-Σ[1,1] = 99
-Σ[2,2] = 9
-Σ[3,3] = 0.3
-Σ[4,4] = 0.01
-
-
-# Step 2: Symmetrize A to get B
-B = U @ Σ @ Vᵀ
-
-# Step 3: Print B[0,0]
-print("Original B[0,0]:", B[0, 0])
-
-# Step 4: Set B[0,0] to 0
-B[0, 0] = 0 #np.mean(B)
-print("Set to mean B[0,0]:", B[0, 0])
+A = genfromtxt('imputation.csv', delimiter=',')
+print('A[0,0] original was 0.0736')
+A[0, 0] = 0 # setting unknow value to 0
 
 # Step 5: Eigendecomposition
-[Ū, σ̄ , V̄ᵀ] = np.linalg.svd(B)  # eigh because B is symmetric
+[Ū, σ̄ , V̄ᵀ] = np.linalg.svd(A)  
 
-# Step 6: Truncate small eigenvalues
-# Keep top k largest magnitude eigenvalues
-k = 4  # we know rank ~10, so this is natural
+# I already printed out the singular values 
+# and identified that the first 4 eigenvectors are core
+k = 4  
 idx = np.argsort(np.abs(σ̄))[::-1]
-σ̄ = σ̄[idx[:k]]
-Ū = Ū[:, idx[:k]]
-V̄ᵀ = V̄ᵀ[idx[:k], :]
+σ̄ = σ̄[idx[:k]]		# truncate the singular values
+Ū = Ū[:, idx[:k]]	# truncate the U 
+V̄ᵀ = V̄ᵀ[idx[:k], :] # truncate the V
 
 # Step 7: Reconstruct B_lowrank = V Σ V^T
 Σ̄  = np.diag(σ̄)
-B̄ = Ū  @ Σ̄ @ V̄ᵀ
+Ā = Ū  @ Σ̄ @ V̄ᵀ
 
-# Step 8: Print reconstructed B[0,0]
-print("Reconstructed B[0,0]:", B̄[0, 0])
+# Step 8: Print reconstructed A[0,0]
+print("Reconstructed A[0,0]:", Ā[0, 0])
 
-# (Optional) Check reconstruction error
-error = np.linalg.norm(B - B̄) / np.linalg.norm(B)
-print("Relative Reconstruction Error:", error)
+
+
+
