453902dab9b88a293069ac84fa1f8b5e8e57c710
endsley <chieh.t.wu@gmail.com>
2025-09-17 15:44:32 -0400

    kk

--- math_foundation/panda3D/point_cloud_solution.py
+++ math_foundation/panda3D/point_cloud_solution.py
@@ -4,7 +4,7 @@ from space import *
 class cloud_of_points(space):
 	def __init__(self):
 		space.__init__(self)
-		self.cloud = point_cloud(self, mean=[2,1,1])
+		self.cloud = point_cloud(mean=[2,1,1])
 		self.accept("m", self.move_cloud)
 
 	def move_cloud(self):
